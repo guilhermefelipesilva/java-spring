6b4077a41a4e140c1f9822468c5ec50efc7ff9af
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2021-05-19 22:25:12 -0300

    cria classe de dominio categoria

--- src/main/java/com/guireis/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/guireis/cursomc/resources/CategoriaResources.java
@@ -1,6 +1,10 @@
 package com.guireis.cursomc.resources;
 
-import org.springframework.beans.factory.config.YamlProcessor.ResolutionMethod;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.guireis.cursomc.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResources {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Hello World";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
