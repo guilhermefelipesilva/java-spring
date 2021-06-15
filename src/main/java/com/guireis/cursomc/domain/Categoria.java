86f6b031743316f9046043f13dbf2b8e0ebb6670
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2021-06-14 22:54:46 -0300

    associaçao de produtos e categoria

--- src/main/java/com/guireis/cursomc/domain/Categoria.java
+++ src/main/java/com/guireis/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.guireis.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +19,9 @@ public class Categoria implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria() {
     }
@@ -40,6 +46,14 @@ public class Categoria implements Serializable {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     @Override
